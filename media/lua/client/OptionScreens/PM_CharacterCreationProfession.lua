08c2af9690cd0107f5f8f9e1244152d352bf1b62
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2025-02-12 19:02:47 +0900

    Add [re-randomizeTraits] logic when unwanted trait is included (NOT WORKING)

--- media/lua/client/OptionScreens/PM_CharacterCreationProfession.lua
+++ media/lua/client/OptionScreens/PM_CharacterCreationProfession.lua
@@ -1,17 +1,32 @@
 -- require('OptionScreens/CharacterCreationProfession');
 -- PM_RandomizeTraits = CharacterCreationProfession:derive("PM_RandomizeTraits")
 
+function PM_dump(o)
+    if type(o) == 'table' then
+        local s = '{ '
+        for k,v in pairs(o) do
+            if type(k) ~= 'number' then k = '"'..k..'"' end
+            s = s .. '['..k..'] = ' .. PM_dump(v) .. ','
+        end
+        return s .. '} '
+    else
+       return tostring(o)
+    end
+end
+
 function CharacterCreationProfession:randomizeTraits() -- {{{
     -- [PM] check the mod works
     print( "Hello world!!! - 1" );
 
     self:resetBuild();
 
+    -- [PM] select random profession
     local size = #self.listboxProf.items;
     local prof = ZombRand(size)+1;
     self.listboxProf.selected = prof;
     self:onSelectProf(self.listboxProf.items[self.listboxProf.selected].item);
 
+    -- [PM] Add initial traits
     local numTraits = ZombRand(5);
     for i=0,numTraits do
         self.listboxTrait.selected = ZombRand(#self.listboxTrait.items)+1;
@@ -21,6 +36,7 @@ function CharacterCreationProfession:randomizeTraits() -- {{{
     local numBadTraits = ZombRand(5);
     for i=0,numBadTraits do
         self.listboxBadTrait.selected = ZombRand(#self.listboxBadTrait.items)+1;
+        print( listboxBadTrait.selected );
         self:onOptionMouseDown(self.addBadTraitBtn);
     end
 
@@ -77,6 +93,22 @@ function CharacterCreationProfession:randomizeTraits() -- {{{
                     self:onOptionMouseDown(self.addTraitBtn);
                 end
             end
+        end
+    end
+
+    -- [PM] if unwanted trait is included
+    print( "Traits num: ", #self.listboxTraitSelected.items );
+
+    for PM_i=1,#self.listboxTraitSelected.items do
+        local PM_item = self.listboxTraitSelected.items[PM_i];
+        local PM_item_title = PM_item["text"];
+        print( PM_item_title );
+
+        if PM_item_title == "팔 절단" or PM_item_title == "청각 장애" or PM_item_title == "비체계적인" or PM_item_title == "불운" or PM_item_title == "대충대충" then
+            print( "!: ", PM_item_title );
+            self:randomizeTraits();
+        else
+            print( "~continue~" );
         end
     end
 end
