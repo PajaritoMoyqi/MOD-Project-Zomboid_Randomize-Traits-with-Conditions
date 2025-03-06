4b284043130dec88ba1b184b40adba8517f3391c
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2025-03-06 17:50:58 +0900

    Declare [first setting options]

--- media/lua/client/PM_RandomizeTraitsModOptionsConfig.lua
+++ media/lua/client/PM_RandomizeTraitsModOptionsConfig.lua
@@ -1,17 +1,35 @@
 PM_RandomizeTraits = PM_RandomizeTraits or {};
 PM_RandomizeTraits.settings = PM_RandomizeTraits.SETTINGS or {};
 
-PM_RandomizeTraits.settings.Test = false;
+PM_RandomizeTraits.settings.Preselect = true;
+PM_RandomizeTraits.settings.HardPreselect = false;
+PM_RandomizeTraits.settings.AutoReRandomize = false;
 
 if ModOptions and ModOptions.getInstance then
   local function onModOptionsApply(optionValues)
-    PM_RandomizeTraits.settings.Test = optionValues.settings.options.Test;
+    PM_RandomizeTraits.settings.Preselect = optionValues.settings.options.Preselect;
+    PM_RandomizeTraits.settings.Preselect = optionValues.settings.options.HardPreselect;
+    PM_RandomizeTraits.settings.AutoReRandomize = optionValues.settings.options.AutoReRandomize;
   end
   local SETTINGS = {
     options_data = {
-      Test = {
-        name = "UI_PM_RandomizeTraits_Options_Test",
-        tooltip = "UI_PM_RandomizeTraits_Options_Test_ToolTip",
+      Preselect = {
+        name = "UI_PM_RandomizeTraits_Options_Preselect",
+        tooltip = "UI_PM_RandomizeTraits_Options_Preselect_ToolTip",
+        default = true,
+        OnApplyMainMenu = onModOptionsApply,
+        OnApplyInGame = onModOptionsApply,
+      },
+      HardPreselect = {
+        name = "UI_PM_RandomizeTraits_Options_HardPreselect",
+        tooltip = "UI_PM_RandomizeTraits_Options_HardPreselect_ToolTip",
+        default = false,
+        OnApplyMainMenu = onModOptionsApply,
+        OnApplyInGame = onModOptionsApply,
+      },
+      AutoReRandomize = {
+        name = "UI_PM_RandomizeTraits_Options_AutoReRandomize",
+        tooltip = "UI_PM_RandomizeTraits_Options_AutoReRandomize_ToolTip",
         default = false,
         OnApplyMainMenu = onModOptionsApply,
         OnApplyInGame = onModOptionsApply,
